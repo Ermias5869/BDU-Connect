43567bc50aff155fa4a66b771ef1c753cfd3c3a4
Ermias5869 <ermiasamare8030@gmail.com>
2025-07-14 03:15:37 +0300

    chore: add .env variables and update Vite proxy config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_SOCKET_URL,
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // Load .env variables
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: env.VITE_SOCKET_URL, // ✅ use env directly
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
